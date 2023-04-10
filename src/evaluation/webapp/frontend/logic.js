05aedbdd66a34f80cb3044201ce42d6edaf6d2e1
blaz-r <blaz.rolih@gmail.com>
2023-04-10 21:27:22 +0200

    Added html frontend

---
+++ src/evaluation/webapp/frontend/logic.js
@@ -0,0 +1,10 @@
+function saveScore() {
+    var score = {
+        "textId": document.getElementById("textId").value,
+        "scorerId": document.getElementById("scorerId").value,
+        "appropriateness": 5,
+        "fluency": 5,
+        "diversity": 42,
+        "overall": 42
+    }
+}
