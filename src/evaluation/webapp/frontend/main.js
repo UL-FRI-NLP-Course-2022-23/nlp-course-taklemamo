69cd9df3294bef9646594581782503103ae0d7a9
Blaž Rolih <blaz.rolih@gmail.com>
2023-04-13 00:37:35 +0200

    Implemented FE logic

---
+++ src/evaluation/webapp/frontend/main.js
@@ -0,0 +1,56 @@
+let i = -1;
+
+async function setText() {
+    // next text is id + num
+    const text = await getText("pre_eval_a" + i)
+    document.getElementById("text_original").innerHTML = text.originalText
+    document.getElementById("text_par").innerHTML = text.paraphrasedText
+    document.getElementById("text_id").innerHTML = text.textId
+}
+
+window.onload = async function() {
+    // save consecutive text id num in local storage
+    if (localStorage.getItem("id_num") != null) {
+        i = localStorage.getItem("id_num")
+    }
+    else {
+        i = 0
+        localStorage.setItem("id_num", i)
+    }
+
+    await setText()
+
+    let form = document.getElementById("text_form");
+    form.addEventListener('submit', handleForm);
+}
+
+async function handleForm(e) { 
+    e.preventDefault(); 
+
+    console.log("submitting score")
+
+    let text_id = $("#text_id").text()
+    let scorer_id = $("#scorer_id").val()
+    let appropriateness = $("#range_apr").val()
+    let fluency = $("#range_flue").val()
+    let diversity = $("#range_div").val()
+    let overall = (parseFloat(appropriateness) + parseFloat(fluency) + parseFloat(diversity)) / 3
+    let score = {
+        textId: text_id,
+        scorerId: scorer_id,
+        appropriateness: appropriateness,
+        fluency: fluency,
+        diversity: diversity,
+        overall: overall
+    }
+
+    // save score to db
+    let status = await postScore(score)
+    console.log(status)
+
+    // next text
+    i++
+    localStorage.setItem("id_num", i)
+
+    await setText()
+} 
