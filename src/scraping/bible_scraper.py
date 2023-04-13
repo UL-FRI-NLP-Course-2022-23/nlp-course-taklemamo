3d2d60b2f688155cfe93a537a45907d49a1b09ba
GregaSustar <grega.sustar10@gmail.com>
2023-04-13 14:54:06 +0200

    Small adjustments for better alignment

--- src/scraping/bible_scraper.py
+++ src/scraping/bible_scraper.py
@@ -4,13 +4,12 @@ import re
 import string
 import unicodedata
 
+POETRY = ['Job', 'Ps', 'Prg', 'Prd', 'Vp', 'Žal']
 
 class BibleScraper():
 
-    def __init__(self, lang='sl'):
-        self.lang = lang
-        self.url = f"https://www.biblija.net/biblija.cgi?l={lang}"
-
+    def __init__(self):
+        self.url = "https://www.biblija.net/biblija.cgi?m=4+Mz+7&id13=1&id14=1&pos=0&set=6&l=sl&idp0=14&idp1=15"
 
     def get_book_refs(self):
         response = requests.get(self.url)
@@ -22,6 +21,7 @@ class BibleScraper():
             t = matches.replace("'", "").split(", ")[1]
             book_refs.append(t)
 
+        book_refs = [bk for bk in book_refs if bk not in POETRY]
         return book_refs
 
 
@@ -38,35 +38,39 @@ class BibleScraper():
         return chapt_refs
 
 
-    def get_paragraphs(self, chapt_ref):
+    def get_verses(self, chapt_ref):
         chapt_url = self.url + f"&m={chapt_ref}"
         response = requests.get(chapt_url)
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        # Remove undesired elements
-        elements_to_remove = soup.find_all(['img', 'a'])
-        elements_to_remove += soup.find_all(class_='opomba-layer')
-        for r in elements_to_remove:
-            r.extract()
+        table = soup.select('table:has(.tr-odd)')[-1]
+        for row in table.find_all('tr'):
+            for col in row.find_all('td', class_='text'):
 
-        paragraphs = {}
-        for i, p in enumerate(soup.find_all(class_='p')):
-            text = unicodedata.normalize('NFKD', p.text).strip()
-            verses = re.findall(r'(\d+) (.+?)(?=\n\d+|$)', text, flags=re.DOTALL)
-            paragraphs[i] = dict(verses)
+                elements_to_keep = soup.find_all(class_=re.compile(r'^(p|v|q\d+)$'))
+                for element in col.find_all():
+                    if element not in elements_to_keep:
+                        element.extract()
 
-        return paragraphs
+                text = unicodedata.normalize('NFKD', col.text).strip()
+
+
+                print(text)
+                print(10*'-')
 
 
 if __name__ == "__main__":
 
-    bs = BibleScraper(lang='sl')
+    bs = BibleScraper()
     book_refs = bs.get_book_refs()
+    print(book_refs)
     chapt_refs = bs.get_chapt_refs(book_refs[0])
-    ps = bs.get_paragraphs(chapt_refs[3])
+    ps = bs.get_verses(chapt_refs[0])
 
-    import pprint
-    pprint.pprint(ps, indent=4)
+    print(ps)
+
+    # import pprint
+    # pprint.pprint(ps, indent=4)
 
 
 
