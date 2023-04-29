39605bcc8922220ff4218d0d4898e5879417861f
blaz-r <blaz.rolih@gmail.com>
2023-04-29 22:04:29 +0200

    Implemented Google BLEU and ROUGEp

--- src/evaluation/metrics/paraphrase_metrics.py
+++ src/evaluation/metrics/paraphrase_metrics.py
@@ -1,9 +1,12 @@
 from parascore import ParaScorer
 from abc import ABC, abstractmethod
 
-import nltk
+from math import pi, cos
 
+import nltk
+import numpy as np
 import evaluate
+
 
 class Metric(ABC):
     def __init__(self):
@@ -40,7 +43,9 @@ class ParaScoreMetric(Metric):
 
     def eval(self, original, paraphrase):
         diversity = self.diverse([paraphrase], [original])
+        print("DS:", diversity)
         similarity = self.scorer.score([paraphrase], [original])
+        print("SIM:", similarity)
 
         out = [x + 0.5 * y for (x, y) in zip(similarity, diversity)]
         return out[0].item()
@@ -76,6 +81,43 @@ class SacreBLEUMetric(Metric):
         return res["score"]
 
 
+class GoogleBLEUMetric(Metric):
+    def __init__(self):
+        super().__init__()
+        self.google_bleu = evaluate.load("google_bleu")
+
+    def eval(self, original, paraphrase):
+        res = self.google_bleu.compute(predictions=[paraphrase], references=[original])
+        return res["google_bleu"]
+
+
+class ROUGEpMetric(Metric):
+    def __init__(self, beta=2, gamma=7):
+        super().__init__()
+        self.rouge = evaluate.load("rouge")
+        self.beta = beta
+        self.gamma = gamma
+
+    def eval(self, original, paraphrase):
+        if type(original) is not list:
+            raise "ROUGEp requires list of all paraphrase pairs"
+        res = self.rouge.compute(predictions=paraphrase, references=original, use_aggregator=False)
+
+        src_rouge_l = np.array(res["rougeL"])
+        src_rouge_1 = np.array(res["rouge1"])
+        bench_rouge = np.mean(src_rouge_l)
+
+        orig_lens = np.array([len(s) for s in original])
+        para_lens = np.array([len(s) for s in paraphrase])
+
+        max_diff = np.maximum(src_rouge_l - bench_rouge, 0)
+        nf = (1 - (max_diff / (1 - bench_rouge)) ** self.beta)
+        ff = (1 - (max_diff / bench_rouge) ** self.gamma)
+        lenpen = np.minimum(1, np.exp(1 - para_lens / orig_lens))
+
+        return src_rouge_1 * nf * ff * lenpen
+
+
 if __name__ == "__main__":
     sentences = [
         "Policisti PU Ljubljana so bili v četrtek zvečer okoli 22.30 obveščeni o ropu v parku Tivoli v Ljubljani. Ugotovili so, da so štirje storilci pristopili do oškodovancev in od njih z nožem v roki zahtevali denar. Ko so jim denar izročili, so storilci s kraja zbežali. Povzročili so za okoli 350 evrov materialne škode.",
@@ -91,6 +133,11 @@ if __name__ == "__main__":
         "Novinarji ameriškega The New York Timesa so v roke prejeli kopijo še neobjavljenega revizijskega poročila, v katerem je skupina Amnesty International neodvisno ugotavljala legitimnost svoje kritike ukrajinske vojske. V avgustovskih obtožbah je namreč neprofitna organizacija za človekove pravice dejala, da ukrajinski vojaki v bojih proti ruskim neupravičeno ogrožajo civiliste. ",
     ]
 
+    refs = [
+        "V četrtek zvečer okol 22.30 so bili policisti PU Ljubljana obveščeni o ropu v parku Tivoli v Ljubljani. Ugotovili so, da so štirje storilci z nožem v roki pristopili do oškodovancev in od njih zatevali denar. Po prejetju denarjo so sotricli pobegnili s kraja. Matreialna škoda nastala v postpku znaša okoli 350 evrov.",
+        "Sobota je bila od začetka jasna, a se je sredi dneva soncu pridružilo nekaj oblakov. Prekmurje je popoldne zajelo hudo neurje z močnim dežjem in sodro, ki je pobelila Lendavo z okolico. Nekaj kreajvnih padavin bo v začetku noči nastopilo tudi drugod po državi."
+    ]
+
     # models = ["cjvt/sleng-bert", "cjvt/sloberta-sleng", "cjvt/sloberta-si-nli", "cjvt/t5-sl-small", "EMBEDDIA/sloberta", "EMBEDDIA/crosloengual-bert"]
     #
     # for m in models:
@@ -103,10 +150,18 @@ if __name__ == "__main__":
     # metric = ParaScoreMetric()
     # metric = BLEUMetric()
     # metric = ROUGEMetric()
-    metric = SacreBLEUMetric()
+    # metric = SacreBLEUMetric()
+    # metric = GoogleBLEUMetric()
+    # metric = RoRoScoreMetric()
 
-    for sen, par, smet in zip(sentences, paraps, smeti):
-        print("Para:", metric.eval(sen, par))
-        print("Copy:", metric.eval(sen, sen))
-        print("Unrelated:", metric.eval(sen, smet))
+    # print("Model: ", metric.eval(sentences[0], paraps[0]))
+    # print("Hand: ", metric.eval(sentences[1], paraps[1]))
+    # print("Copy: ", metric.eval(sentences[1], sentences[1]))
 
+    # for sen, par, smet in zip(sentences, paraps, smeti):
+    #     print("Para:", metric.eval(sen, par))
+    #     print("Copy:", metric.eval(sen, sen))
+    #     print("Unrelated:", metric.eval(sen, smet))
+
+    metric = ROUGEpMetric()
+    print(metric.eval(sentences * 4, paraps + sentences + refs + smeti))
